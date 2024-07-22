39e53c5b1d5feea8036ffe2ce0a289ed8f019e53
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2024-07-22 19:40:27 +0530

    singleton prisma client,server action data fetching in next async components.

--- src/app/actions/user.ts
+++ src/app/actions/user.ts
@@ -1,6 +1,8 @@
 "use server"
 import client from "@/db"
 
+
+//server action - can be called from client or on server side.
 export async function signup(email:string,password:string) {
     try{
         await client.user.create({
